70c0e69a87723fa3eed350821438eb4960b4399a
silifonov <silifonov@gmail.com>
2019-07-27 17:43:48 +0300

    Creating custom CMS Block, showing it in modal

---
+++ app/code/Silifonov/ModalBlock/Setup/InstallData.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Silifonov\ModalBlock\Setup;
+
+use Magento\Cms\Model\BlockFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+
+class InstallData implements InstallDataInterface
+{
+    private $blockFactory;
+
+    public function __construct(BlockFactory $blockFactory)
+    {
+        $this->blockFactory = $blockFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $cmsBlockData = [
+            'title' => 'Modal CMS Block',
+            'identifier' => 'modal_cms_block',
+            'content' => "<h1>Modal CMS block</h1>",
+            'is_active' => 1,
+            'stores' => [0],
+            'sort_order' => 0
+        ];
+
+        $this->blockFactory->create()->setData($cmsBlockData)->save();
+    }
+}
